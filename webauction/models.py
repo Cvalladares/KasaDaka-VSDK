1b6464ce3555911afa9737d473226393661632ff
Maaike Maas <maaike.maas@procurios.eu>
2018-05-03 23:46:32 +0200

    Finished web auction interface

--- webauction/models.py
+++ webauction/models.py
@@ -2,9 +2,22 @@ from django.db import models
 from django.utils import timezone
 
 
+#class Schedule(models.Model):
+#    datetime = models.DateTimeField('date of auction')
+#    item = models.CharField(max_length=200)
+#    nameofseller = models.CharField(max_length=200,default='john doe')
+#    startingprice = models.IntegerField(default=1)
+
+
+#    def __str__(self):
+#            return self.nameofseller
+
+
+
 class Bid(models.Model):
-    name = models.CharField(max_length=200, default='noName')
-    bid = models.IntegerField(default=1)
+#    schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE)
+    potentialbuyer = models.CharField(max_length=200, default='noName')
+    offerinzedi = models.IntegerField(default=1)
     nrofitems = models.IntegerField(default=1)
 
     def __str__(self):
@@ -19,5 +32,8 @@ class Item(models.Model):
     def __str__(self):
             return self.name
 
-# from webauction.models import Bid, Item
+
+
+
+# from webauction.models import Shedule, Bid, Item
 # from django.utils import timezone
