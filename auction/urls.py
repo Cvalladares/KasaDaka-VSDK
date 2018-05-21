569b6b427a9b0c0d54c238cc7c1c1a54563ec0ef
Jay <sanjaysheombar@gmail.com>
2018-05-22 01:16:00 +0200

    trying something

--- auction/urls.py
+++ auction/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('product', views.product, name='product'),
     path('bid', views.bid, name='bid'),
     path('vxml', views.vxml, name='vxml'),
+    path('voice', views.voice, name='voice'),
     path('new_product', views.add_new_product, name='add_new_product'),
     path('new_auction', views.create_new_auction, name='create_new_auction'),
     path('place_bid', views.place_bid, name='place_bid'),
